21309d144d7151780a61006882816bd61584d2a2
blossom.efosa <blossom.efosa@globalaccelerex.com>
2024-06-18 08:03:37 +0100

    Getting started

--- src/com/calc/Main.java
+++ src/com/calc/Main.java
@@ -3,6 +3,7 @@ package com.calc;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calculator Mode".toUpperCase());
+        System.out.println("Let's get started!");
         App calc = new App();
         calc.run();
 
